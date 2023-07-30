02b17d731d810911628dc096e745a28fa371653e
Max Souza <max.souza@overdose.digital>
2023-07-30 10:33:25 -0300

    Added copyright doc

--- Model/Resolver/Identity.php
+++ Model/Resolver/Identity.php
@@ -1,4 +1,11 @@
-<?php declare(strict_types=1);
+<?php 
+
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
 
 namespace Discorgento\ProductLastOrder\Model\Resolver;
 
